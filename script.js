a59e9911ad525c2d7e4c918df496d54bc1542c65
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-05 19:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let arr=str.split("");
+	arr.sort();
+	let c=arr[0];
+	let count=1;
+	for(let i=1;i<arr.length;i++){
+		if(arr[i]==c){
+			count++;
+			
+		}
+		else{
+			if(count==1){ 
+				return c;
+			}
+			count=1;
+			c=arr[i];
+		}
+	}
+	if(count==1){
+		return c;
+	}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
